1f6bd120de19818ff63e9f32227873c562b5b240
Neha Gupta <elcicneha@gmail.com>
2026-04-14 18:00:46 +0530

    Added more supported file types

--- QuickLookCode/QuickLookCodeShared/Renderers/FileTypeRegistry.swift
+++ QuickLookCode/QuickLookCodeShared/Renderers/FileTypeRegistry.swift
@@ -32,8 +32,12 @@ public enum FileTypeRegistry {
 
         // Web — core
         "js":       .init(grammarSearch: "javascript",       displayName: "JavaScript"),
+        "mjs":      .init(grammarSearch: "javascript",       displayName: "JavaScript Module"),
+        "cjs":      .init(grammarSearch: "javascript",       displayName: "CommonJS Module"),
         "jsx":      .init(grammarSearch: "javascriptreact",  displayName: "JSX"),
         "ts":       .init(grammarSearch: "typescript",       displayName: "TypeScript"),
+        "mts":      .init(grammarSearch: "typescript",       displayName: "TypeScript ES Module"),
+        "cts":      .init(grammarSearch: "typescript",       displayName: "TypeScript CJS Module"),
         "tsx":      .init(grammarSearch: "typescriptreact",  displayName: "TSX"),
         "html":     .init(grammarSearch: "html",             displayName: "HTML"),
         "htm":      .init(grammarSearch: "html",             displayName: "HTML"),
@@ -53,10 +57,12 @@ public enum FileTypeRegistry {
 
         // Scripting
         "py":       .init(grammarSearch: "python",           displayName: "Python"),
+        "pyi":      .init(grammarSearch: "python",           displayName: "Python Type Stub"),
         "rb":       .init(grammarSearch: "ruby",             displayName: "Ruby"),
         "sh":       .init(grammarSearch: "shellscript",      displayName: "Shell"),
         "bash":     .init(grammarSearch: "shellscript",      displayName: "Bash"),
         "zsh":      .init(grammarSearch: "shellscript",      displayName: "Zsh"),
+        "ksh":      .init(grammarSearch: "shellscript",      displayName: "Korn Shell"),
         "fish":     .init(grammarSearch: "fish",             displayName: "Fish"),
         "ps1":      .init(grammarSearch: "powershell",       displayName: "PowerShell"),
         "lua":      .init(grammarSearch: "lua",              displayName: "Lua"),
@@ -79,15 +85,16 @@ public enum FileTypeRegistry {
         "vb":       .init(grammarSearch: "vb",               displayName: "Visual Basic"),
 
         // Data / Config
-        "json":     .init(grammarSearch: "json",             displayName: "JSON"),
-        "jsonc":    .init(grammarSearch: "jsonc",            displayName: "JSON with Comments"),
-        "yaml":     .init(grammarSearch: "yaml",             displayName: "YAML"),
-        "yml":      .init(grammarSearch: "yaml",             displayName: "YAML"),
-        "toml":     .init(grammarSearch: "toml",             displayName: "TOML"),
-        "xml":      .init(grammarSearch: "xml",              displayName: "XML"),
-        "plist":    .init(grammarSearch: "xml",              displayName: "Property List"),
-        "ini":      .init(grammarSearch: "ini",              displayName: "INI"),
-        "env":      .init(grammarSearch: "dotenv",           displayName: "Env"),
+        "json":       .init(grammarSearch: "json",           displayName: "JSON"),
+        "jsonc":      .init(grammarSearch: "jsonc",          displayName: "JSON with Comments"),
+        "yaml":       .init(grammarSearch: "yaml",           displayName: "YAML"),
+        "yml":        .init(grammarSearch: "yaml",           displayName: "YAML"),
+        "toml":       .init(grammarSearch: "toml",           displayName: "TOML"),
+        "xml":        .init(grammarSearch: "xml",            displayName: "XML"),
+        "plist":      .init(grammarSearch: "xml",            displayName: "Property List"),
+        "ini":        .init(grammarSearch: "ini",            displayName: "INI"),
+        "properties": .init(grammarSearch: "ini",            displayName: "Java Properties"),
+        "env":        .init(grammarSearch: "dotenv",         displayName: "Env"),
         "editorconfig": .init(grammarSearch: "editorconfig", displayName: "EditorConfig"),
 
         // Query
@@ -102,6 +109,8 @@ public enum FileTypeRegistry {
         "proto":    .init(grammarSearch: "proto3",           displayName: "Protobuf"),
         "makefile": .init(grammarSearch: "make",             displayName: "Makefile"),
         "mk":       .init(grammarSearch: "make",             displayName: "Makefile"),
+        "bat":      .init(grammarSearch: "bat",              displayName: "Windows Batch"),
+        "cmd":      .init(grammarSearch: "bat",              displayName: "Windows Command"),
 
         // Other popular
         "php":      .init(grammarSearch: "php",              displayName: "PHP"),
@@ -112,7 +121,9 @@ public enum FileTypeRegistry {
         "erl":      .init(grammarSearch: "erlang",           displayName: "Erlang"),
         "clj":      .init(grammarSearch: "clojure",          displayName: "Clojure"),
         "cljs":     .init(grammarSearch: "clojure",          displayName: "ClojureScript"),
+        "cljc":     .init(grammarSearch: "clojure",          displayName: "Clojure Common"),
         "hs":       .init(grammarSearch: "haskell",          displayName: "Haskell"),
+        "lhs":      .init(grammarSearch: "haskell",          displayName: "Literate Haskell"),
         "ml":       .init(grammarSearch: "ocaml",            displayName: "OCaml"),
         "mli":      .init(grammarSearch: "ocaml",            displayName: "OCaml Interface"),
         "jl":       .init(grammarSearch: "julia",            displayName: "Julia"),
@@ -128,5 +139,20 @@ public enum FileTypeRegistry {
         "hlsl":     .init(grammarSearch: "hlsl",             displayName: "HLSL"),
         "vert":     .init(grammarSearch: "glsl",             displayName: "GLSL Vertex"),
         "frag":     .init(grammarSearch: "glsl",             displayName: "GLSL Fragment"),
+        "geom":     .init(grammarSearch: "glsl",             displayName: "GLSL Geometry Shader"),
+        "tesc":     .init(grammarSearch: "glsl",             displayName: "GLSL Tess. Control Shader"),
+        "tese":     .init(grammarSearch: "glsl",             displayName: "GLSL Tess. Eval. Shader"),
+        "comp":     .init(grammarSearch: "glsl",             displayName: "GLSL Compute Shader"),
+
+        // Templates
+        "hbs":          .init(grammarSearch: "handlebars",   displayName: "Handlebars"),
+        "handlebars":   .init(grammarSearch: "handlebars",   displayName: "Handlebars"),
+        "pug":          .init(grammarSearch: "pug",          displayName: "Pug"),
+        "jade":         .init(grammarSearch: "pug",          displayName: "Pug (Jade)"),
+
+        // Markup / Docs
+        "tex":      .init(grammarSearch: "latex",            displayName: "LaTeX"),
+        "latex":    .init(grammarSearch: "latex",            displayName: "LaTeX"),
+        "sty":      .init(grammarSearch: "latex",            displayName: "LaTeX Style"),
     ]
 }
